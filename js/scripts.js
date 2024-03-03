191410576e0d89a25a9795bf5aadc12a85161f7b
onlyfansdownloader <onlyfansvideodownloader@gmail.com>
2024-03-03 17:14:37 +0800

    init GA and mutil language

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,26 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 function goDownloadEXT() {
-  // alert("1")
-  // window.open("http://127.0.0.1:5500/web/OfDFloder.rar")
 
-  // 构造文件的URL
+
+
   var fileUrl = "assets/OfDFloder.rar";
 
-  // 创建一个虚拟的<a>元素
+
   var link = document.createElement("a");
   link.href = fileUrl;
 
-  // 设置下载文件的属性
+
   link.download = "OfDFloder.rar";
 
-  // 将虚拟<a>元素添加到DOM中
+
   document.body.appendChild(link);
 
-  // 模拟点击<a>元素，开始下载文件
   link.click();
 
-  // 下载完成后移除<a>元素
+
   document.body.removeChild(link);
-  //  alert(2)
+
 
   log("downloadExt")
 
@@ -35,22 +33,21 @@ function goDownloadEXT() {
 
 function copyToClipboard() {
   var email = "onlyfansdownloader@gmail.com";
-  // 创建一个包含固定值的临时元素
+
   var tempInput = document.createElement("input");
   tempInput.value = email;
   document.body.appendChild(tempInput);
 
-  // 选中临时元素中的文本
+
   tempInput.select();
 
-  // 复制选中的文本到剪贴板
+
   document.execCommand("copy");
 
-  // 移除临时元素
+
   document.body.removeChild(tempInput);
 
-  // 提示用户已成功复制
-  // alert("Email address has been copied to your clipboard");
+
 
   log("copyEmail")
 }
@@ -62,34 +59,30 @@ function log(inaction) {
   let action = inaction;
 
 
-  // 创建XMLHttpRequest对象
   var xhr = new XMLHttpRequest();
 
-  // 准备FormData对象，用于包装请求数据
+
   var formData = new FormData();
   formData.append('channelId', channelId);
   formData.append('action', inaction);
 
-  // 配置请求
-  xhr.open('POST', 'https://ofdownloader.online/svr/onlyfans/downloader/market/log', true); // 使用POST方法请求某个URL
 
-  // 设置请求完成后的回调函数
+  xhr.open('POST', 'https://ofdownloader.online/svr/onlyfans/downloader/market/log', true);
+
   xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
-      // 请求成功，处理返回的数据
+
       console.log(xhr.responseText);
     } else {
-      // 请求失败
+
       console.error('请求失败：' + xhr.status);
     }
   };
 
-  // 设置请求错误时的回调函数
   xhr.onerror = function () {
     console.error('请求出错');
   };
 
-  // 发送请求，传入FormData对象
   xhr.send(formData);
 
 }
@@ -107,7 +100,33 @@ function getParameterByName(name, url) {
 }
 
 
+let languages = new Array("Enlish", "français", "español", "italiano");
 
+function changeLan(lan) {
+  //navigate to the lan page
+ console.log("change lan")
+
+var currentPageURL = window.location.href;
+
+
+var pathSegments = currentPageURL.split('/');
+
+console.log(pathSegments[3])
+var newURL ="";
+if(pathSegments.length<=4)
+{
+  pathSegments[3]=lan+"/index.html"
+}
+else
+{
+  pathSegments[3] = lan;
+}
+newURL = pathSegments.join('/');
+
+console.log(newURL)
+// window.location.href=newURL;
+
+}
 
 
 
